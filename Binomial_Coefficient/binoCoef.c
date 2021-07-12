c302116a93c9a4270c527b5840b3471594ee3fb8
Aditya Satpathy <adityansx@gmail.com>
2021-07-12 12:41:37 +0530

    added binoCoef.c
    Program to find Binomial Coefficient
    The program uses recursion to find factorial of
    n, r and n-r in the formula n!/r!*(n-r)!

---
+++ Binomial_Coefficient/binoCoef.c
@@ -0,0 +1,24 @@
+/*C Program to find binomial coefficient
+
+Binomial Coefficient can be defined as the 
+coefficient of x^r in the expansion of (1+x)^n.
+*/
+long int fact(long int a)
+{
+	if (a == 0)
+		return 1;
+	else
+		return a * fact(a - 1);
+}
+
+main()
+{
+	int n, r;
+	printf("Enter n: ");
+	scanf("%d", &n);
+
+	printf("Enter r: ");
+	scanf("%d", &r);
+
+	printf("The value of C(%d, %d) is %ld\n", n, r, fact(n) / (fact(r) * fact(n - r)));
+}
