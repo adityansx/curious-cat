3bb308430349c7e03dcf3df650a676ea771f2d75
Aditya Satpathy <adityansx@gmail.com>
2021-07-23 12:30:49 +0530

    arrayoperations.c: added option to perform more operations on the entered array
    Added option to perform more operation on the entered array after one
    operation is completed.

--- Array/arrayoperations.c
+++ Array/arrayoperations.c
@@ -7,7 +7,7 @@ void reverse(int*, int);
 
 main()
 {
-	int n, choice;
+	int n, choice, choice2;
 
 	system("clear");
 //	printf("\e[H\e[2J\e[3J");//clears the screen
@@ -23,47 +23,57 @@ main()
 	for(int a = 0; a < n; a++)
 		scanf("%d", &array[a]);
 
-	printf("\nNow enter your choice. Press\n1   to print the even valued elements\n2   to print the odd valued elemenets\n3   print the sum and average of the elements\n    of the array\n4   print the maximum and minimum element\n    of the array\n5   to remove duplicate elements from the array\n6   to print the array in reverse order\n7   to re-enter the array\n0   to exit\n\n");
-	scanf("%d", &choice);
-	printf("\n");
-
-	switch(choice)
+	do
 	{
-		case 0:
-			system("exit");
-			break;
+		printf("\nNow enter your choice. Press\n1   to print the even valued elements\n2   to print the odd valued elemenets\n3   print the sum and average of the elements\n    of the array\n4   print the maximum and minimum element\n    of the array\n5   to remove duplicate elements from the array\n6   to print the array in reverse order\n7   to re-enter the array\n0   to exit\n\n");
+		scanf("%d", &choice);
+		printf("\n");
+
+		switch(choice)
+		{
+			case 0:
+				system("exit");
+				break;
 			
-		case 1:
-			print_even(array, n);
-			break;
+			case 1:
+				print_even(array, n);
+				break;
 
-		case 2:
-			print_odd(array, n);
-			break;
+			case 2:
+				print_odd(array, n);
+				break;
 
-		case 3:
-			average(array, n);
-			break;
+			case 3:
+				average(array, n);
+				break;
 
-		case 4:
-			max_min(array, n);
-			break;
+			case 4:
+				max_min(array, n);
+				break;
 
-		case 5:
-			duplicate(array, n);
-			break;
+			case 5:
+				duplicate(array, n);
+				break;
 
-		case 6:
-			reverse(array, n);
-			break;
+			case 6:
+				reverse(array, n);
+				break;
 
-		case 7:
+			case 7:
+				main();
+				break;
+
+			default:
+				printf("Invalid Input!\n");
+		}	
+		printf("\nEnter 1   for more operations on the entered array\n      2   to re-enter array\n      0   to exit\n");
+		scanf("%d", &choice2);
+
+		if(choice2 == 2)
 			main();
-			break;
-
-		default:
-			printf("Invalid Input!\n");
 	}
+
+	while(choice2 != 0);	
 }
 void print_even(int *array, int n)
 {
